e142fb1d5fd140099d1fd8fef1ccbe9dc65057d1
Jonathan Fambro <fambrotekproductions@gmail.com>
2024-12-01 12:33:02 -0500

    Revert back to original where component styles and templates are separated

--- joes-robot-shop/src/home/home.component.ts
+++ joes-robot-shop/src/home/home.component.ts
@@ -2,15 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'bot-home',
-  standalone: true,
   imports: [],
-  template: ` <p class="red">Inline home works!</p> `,
-  styles: [
-    `
-      .red {
-        color: red;
-      }
-    `,
-  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {}
