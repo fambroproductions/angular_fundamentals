5b320d96c72fe831c10946794aee2e4adab29932
Jonathan Fambro <fambrotekproductions@gmail.com>
2024-12-01 18:34:30 -0500

    Fixed warning issue

--- joes-robot-shop/src/app/app.component.ts
+++ joes-robot-shop/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  
-}
+export class AppComponent {}
