595d61da0fd34e7530724901c3147193505fe333
Jonathan Fambro <fambrotekproductions@gmail.com>
2024-12-01 19:07:17 -0500

    Creating Additional Components

---
+++ joes-robot-shop/src/app/catalog/catalog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bot-catalog',
+  standalone: true,
+  imports: [],
+  templateUrl: './catalog.component.html',
+  styleUrl: './catalog.component.css',
+})
+export class CatalogComponent {}
